45ce1e497a75358b2874cf639e4962c01497837a
junyoek <106368769+junyoek@users.noreply.github.com>
2023-03-31 23:38:11 +0900

    Add files via upload
    프로젝트 생성, H2 Database 연결

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.FactoryBasedNavigableListAssert;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+//@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+
+        //Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        //Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성보장
+    }
+}
